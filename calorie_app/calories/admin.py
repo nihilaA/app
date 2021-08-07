099bb5e19c8e15d4530e8a435ef0b963785f0f39
nihilaA <nihilaarivalahan001@gmail.com>
2021-08-07 20:04:41 +0530

    first commit

---
+++ calorie_app/calories/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Food,Profile,PostFood
+
+class ProfileAdmin(admin.ModelAdmin):
+    readonly_fields = ('date',)
+
+admin.site.register(Food)
+admin.site.register(Profile)
+admin.site.register(PostFood)
+
+# Register your models here.
